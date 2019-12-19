025e70f8587eebaad9baa8e3ba133a254b454c33
Trevor Roberts Jr <trevorr@amazon.com>
2019-12-19 02:37:44 -0500

    quieter apt installs and update to gazebo9

--- setup_host/basics.sh
+++ setup_host/basics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get install -y emacs lsof wget curl git htop less build-essential terminator make cmake net-tools lubuntu-desktop xvfb
+apt-get update -qqy
+apt-get install -qqy emacs lsof wget curl git htop less build-essential terminator make cmake net-tools lubuntu-desktop xvfb
